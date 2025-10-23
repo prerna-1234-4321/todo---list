cd1afbb3e8c76dc74d62f0efe79178badcd76a82
prerna-1234-4321 <prernakumari9612@gmail.com>
2025-10-23 19:13:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+js
+function addTask() {
+  let input = document.getElementById('taskInput');
+  let task = input.value.trim();
+
+  if (task === '') {
+    alert('Please enter a task!');
+    return;
+  }
+
+  let ul = document.getElementById('taskList');
+  let li = document.createElement('li');
+  li.textContent = task;
+
+  li.onclick = function() {
+    ul.removeChild(li);
+  };
+
+  ul.appendChild(li);
+  input.value = '';
+}
+
